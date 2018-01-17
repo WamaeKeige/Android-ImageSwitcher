12625194d627efecad197662f5b2843e83290230
Wamae Keige <mkeige@gmail.com>
2018-01-17 11:43:42 +0300

    ImageSwitcher

---
+++ app/src/main/java/com/awake/imageswitcher/Car.java
@@ -0,0 +1,14 @@
+package com.awake.imageswitcher;
+
+/**
+ * Created by Administrator on 17/01/2018.
+ */
+
+public class Car {
+    static int [] carimage= {R.drawable.gtrold, R.drawable.gtrxx, R.drawable.mazdarx, R.drawable.mazdarxfive,
+            R.drawable.peugeot,R.drawable.subaruwrx
+    };
+    static String[] carnames = {"1973Nissan Skyline Gtr", "1999 Nissan Gtr",
+            "Mazda Rx3", "Mazda Rx5", "Peugeot 504", "Subaru WRX STI"};
+
+}
